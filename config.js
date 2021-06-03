6c41432ef22abe2b6f308a63957df0eea78879ba
reek188 <80512977+reek188@users.noreply.github.com>
2021-06-03 09:07:17 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+const {
+	prefix,
+	token,
+} = require('./config.json');
+const ytdl = require('ytdl-core');
+
+const client = new Discord.Client();
+client.login(token);
+
